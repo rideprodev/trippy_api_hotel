4228eb1dd594086ba9ac5ffb9ca70a97d9c822d3
Sonu P <sonu@mobitplus.com>
2024-12-16 18:03:58 +0530

    update the refund functionality

--- src/repositories/bookingRepository.js
+++ src/repositories/bookingRepository.js
@@ -58,6 +58,7 @@ export default {
             "hotelCode",
             "cityCode",
             "cancelByDate",
+            "platformPaymentStatus",
             "reavalidateResponse",
           ],
           model: HotelBooking,
